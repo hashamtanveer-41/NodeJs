54cb039daa87a2d9f8a4652ee0a5d0fb8a7f7b18
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-06-17 04:05:15 +0500

    methods on node js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const uri = require("url")
 
 const myServer = http.createServer((req ,res)=>{
     if (req.url === "/favicon.ico")return res.end()
-    const log = `${Date.now()}: ${req.url} New request received \n `
+    const log = `${Date.now()}: ${req.method} ${req.url} New request received \n `
     console.log(uri.Url)
     fs.appendFile("log.txt",log, (err, data) =>{
         res.end("Req at "+ req.url)
