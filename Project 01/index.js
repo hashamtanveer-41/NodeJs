9973ded38e5f8714a6287c8b065937e08c3d9683
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-06-17 19:40:20 +0500

    feat: implement user management with MongoDB integration and update API endpoints

--- Project 01/index.js
+++ Project 01/index.js
@@ -31,11 +31,12 @@ const userSchema = new mongoose.Schema({
     gender: {
         type: String,
     }
-})
+}, {timestamps: true})
 const User = mongoose.model("user", userSchema)
 
 
 app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 
 app.use((req, res, next)=>{
     fs.appendFile("log.txt", `\n ${Date.now()}: ${req.ip} ${req.method}: ${req.path}`, (err, data)=>{
@@ -43,42 +44,50 @@ app.use((req, res, next)=>{
     })
 })
 
-app.get("/api/users", (req, res ) => {
-    return res.json(users)
+app.get("/users", async (req, res ) => {
+    const allDbUsers = await User.find({})
+    const html = `
+        <ul>
+            ${allDbUsers.map((user)=>`<li>${user.firstName} - ${user.email}</li>`).join("")}
+        </ul>
+    `;
+    return res.send(html)
+})
+app.get("/api/users", async (req, res ) => {
+    const allDbUsers = await User.find({})
+
+    return res.json(allDbUsers)
 })
 
 app.post("/api/users", async (req, res)=> {
     const body = req.body;
-    if (!body || !body.first_name || !body.email || !body.gender || !body.job_title || !body.lastname_name ){
+    console.log(body)
+    if (!body || !body.firstName || !body.email || !body.gender || !body.job_title || !body.lastName ){
         return res.status(400).json({msg: "All fields are required"})
     }
-    users.push({...body, id:users.length+1})
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data)=>{
-        return res.status(201).json(({status: "success", id: users.length}));
+    const result = await User.create({
+        firstName: body.firstName,
+        email: body.email,
+        gender: body.gender,
+        job_title: body.job_title,
+        lastName: body.lastName,
     })
+    return res.status(201).json({msg: "success"})
 })
 
 app.route("/api/users/:id")
-    .get((req, res ) => {
-        const id =Number(req.params.id);
-        const user = users.find((user) => user.id === id)
-        if(!user || Object.keys(user).length === 0) return res.status(404).json({error: "User not found"})
+    .get(async (req, res ) => {
+        const user = await User.findById(req.params.id)
+        if(!user) return res.status(404).json({error: "User not found"})
         return res.json(user)
     })
-    .patch((req, res)=> {
-        const id =Number(req.params.id);
-        const body = req.body;
-        let user = users.find((user) => user.id === id)
-        user = body;
-        users.find((user) => user[id] === id)
-        return res.json({status: "Pending"})
+    .patch(async (req, res)=> {
+        await User.findByIdAndUpdate(req.params.id, {lastName: "Changed"})
+        return res.json({status: "Success"})
     })
-    .delete((req, res)=> {
-        const id =Number(req.params.id);
-        const users1 = users.filter((user)=>user.id!==id)
-        fs.writeFile("./MOCK_DATA.json", JSON.stringify(users1), (err, data)=>{
-            return res.json(({status: "deleted", id: id}));
-        })
+    .delete(async (req, res)=> {
+        await User.findByIdAndDelete(req.params.id)
+        return res.json(({status: "deleted"}));
     })
 
 app.listen(PORT, ()=>console.log("Server Started successfully on port "+PORT))
