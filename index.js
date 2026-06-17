9bdd1fd44ec56063b043860b9976d3626d8d1e1c
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-06-17 15:43:24 +0500

    refactor: replace http server with Express and add basic routes

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const http =require("http")
 const fs = require("fs");
 const uri = require("url")
+const express = require("express")
 
-const myServer = http.createServer((req ,res)=>{
-    if (req.url === "/favicon.ico")return res.end()
-    const log = `${Date.now()}: ${req.method} ${req.url} New request received \n `
-    console.log(uri.Url)
-    fs.appendFile("log.txt",log, (err, data) =>{
-        res.end("Req at "+ req.url)
-    })
-});
+const app = express();
+app.get("/", (req, res) => {
+    return res.send("Hello From Home page")
+})
+app.get("/about", (req, res) => {
+    return res.send("Hello From About page")
+})
+
+const myServer = http.createServer(app);
 
 myServer.listen(8000, ()=> console.log("Server Started"))
