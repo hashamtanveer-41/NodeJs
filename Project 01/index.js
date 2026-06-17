3bebd63befd4947a68d1922817abf660264dd021
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-06-17 18:02:38 +0500

    feat: integrate MongoDB with Mongoose and enhance user API with validation

--- Project 01/index.js
+++ Project 01/index.js
@@ -1,21 +1,60 @@
 const express = require("express")
 const users = require("./MOCK_DATA.json")
 const fs = require("fs")
+const mongoose = require("mongoose")
 
 const app = express();
 const PORT = 8000;
 
+//Connection
+mongoose.connect("mongodb://127.0.0.1:27017/youtube-app-1")
+    .then(()=>"MongoDb connected successfully!")
+    .catch((error)=>console.log(error))
+
+// Schema
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    jobTitle: {
+        type: String,
+    },
+    gender: {
+        type: String,
+    }
+})
+const User = mongoose.model("user", userSchema)
+
+
 app.use(express.urlencoded({extended: false}))
+
+app.use((req, res, next)=>{
+    fs.appendFile("log.txt", `\n ${Date.now()}: ${req.ip} ${req.method}: ${req.path}`, (err, data)=>{
+        next()
+    })
+})
 
 app.get("/api/users", (req, res ) => {
     return res.json(users)
 })
 
-app.post("/api/users", (req, res)=> {
+app.post("/api/users", async (req, res)=> {
     const body = req.body;
+    if (!body || !body.first_name || !body.email || !body.gender || !body.job_title || !body.lastname_name ){
+        return res.status(400).json({msg: "All fields are required"})
+    }
     users.push({...body, id:users.length+1})
     fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data)=>{
-        return res.json(({status: "success", id: users.length}));
+        return res.status(201).json(({status: "success", id: users.length}));
     })
 })
 
@@ -23,6 +62,7 @@ app.route("/api/users/:id")
     .get((req, res ) => {
         const id =Number(req.params.id);
         const user = users.find((user) => user.id === id)
+        if(!user || Object.keys(user).length === 0) return res.status(404).json({error: "User not found"})
         return res.json(user)
     })
     .patch((req, res)=> {
@@ -30,7 +70,7 @@ app.route("/api/users/:id")
         const body = req.body;
         let user = users.find((user) => user.id === id)
         user = body;
-        users.find((user) => user.id === id)
+        users.find((user) => user[id] === id)
         return res.json({status: "Pending"})
     })
     .delete((req, res)=> {
