1c0c8c2370e1f860deffb4f6c0f3c22bf1723f4d
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-06-19 18:01:06 +0500

    feat: update authentication middleware to use JWT for user session management

--- URLShortener/middlewares/auth.js
+++ URLShortener/middlewares/auth.js
@@ -2,17 +2,20 @@ const {getUser} = require("../service/auth");
 
 
 async function restrictToLoggedInUserOnly(req, res, next){
-    const userUid = req.cookies?.uid;
+    const userUid = req.headers["authorization"];
     if (!userUid)return res.redirect("/login")
-    const user = getUser(userUid);
+    const token = userUid.split("Bearer ")[1]
+    const user = getUser(token);
     if (!user)return res.redirect("/login")
 
     req.user= user;
     next();
 }
 async function checkAuth(req, res, next){
-    const userUid = req.cookies?.uid;
-    req.user= getUser(userUid);
+    console.log(req.headers)
+    const userUid = req.headers["authorization"];
+    const token = userUid.split("Bearer ")[1]
+    req.user= getUser(token);
     next();
 }
 
--- URLShortener/service/auth.js
+++ URLShortener/service/auth.js
@@ -1,11 +1,19 @@
-const sessionIdToUserMap = new Map();
-
-function setUser (id, user){
-    sessionIdToUserMap.set(id, user);
+const jwt = require("jsonwebtoken")
+const secret = "Adfa42341kasd-!@@!#"
+function setUser ( user){
+    return jwt.sign({
+        id: user._id,
+        email: user.email
+    }, secret)
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+function getUser(token){
+    if(!token)return  null;
+    try{
+        return jwt.verify(token, secret);
+    }catch(error){
+        return null;
+    }
 }
 
 module.exports = {
