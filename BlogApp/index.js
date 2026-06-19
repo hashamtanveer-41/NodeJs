820bd831170e10a9da7f08e809bf2ebda7a77a55
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-06-20 04:15:45 +0500

    feat: implement blog listing on homepage and set up blog routes

--- BlogApp/index.js
+++ BlogApp/index.js
@@ -4,16 +4,29 @@ const app = express();
 const PORT = 8000;
 const mongoose = require("mongoose")
 const userRoute = require("./routes/user")
+const blogRoute = require("./routes/blog")
+const cookieParser = require("cookie-parser")
+const {checkForAuthenticationCookie} = require("./middleware/authentication");
 
 app.set('view engine', "ejs")
 app.set("views", path.resolve("./views"))
 app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+app.use(express.static(path.resolve("./public")))
+const Blog = require("./models/blog")
+
+app.use(checkForAuthenticationCookie("token"))
 mongoose.connect('mongodb://127.0.0.1:27017/blogify').then((e)=>console.log("MongoDb connected"))
 
-app.get("/", (req, res)=>{
-    return res.render("home")
+app.get("/", async (req, res)=>{
+    const allBlogs = await Blog.find({});
+    return res.render("home", {
+        user: req.user,
+        blogs: allBlogs,
+    })
 })
 
 app.use("/user", userRoute)
+app.use("/blog", blogRoute)
 
 app.listen(PORT, ()=>console.log("Server started on PORT "+PORT))
