a1f60bb3d060d2059149660456970682bba94919
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-06-20 04:17:29 +0500

    feat: add logout route and clear token cookie

--- BlogApp/routes/user.js
+++ BlogApp/routes/user.js
@@ -29,4 +29,8 @@ router.post("/signup",async (req,res)=>{
     });
     return res.redirect("/")
 })
+
+router.get("/logout", (req, res)=>{
+     res.clearCookie("token").redirect("/")
+})
 module.exports = router;
