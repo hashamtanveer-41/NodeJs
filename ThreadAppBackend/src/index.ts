521518b8a7424e1b5e275932b3ae07813e73bcbf
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-06-23 18:10:55 +0500

    feat: enhance GraphQL server with user context and add getCurrentLoggedInUser query

--- ThreadAppBackend/src/index.ts
+++ ThreadAppBackend/src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 // @ts-ignore
 import {expressMiddleware} from "@as-integrations/express5";
 import createApolloGraphqlServer from "./graphql/index.js";
+import UserService from "./services/user.js";
 
 async function init(){
 
@@ -15,7 +16,17 @@ async function init(){
         res.json({message: "Server is up and running" })
     })
 
-    app.use("/graphql", expressMiddleware(await createApolloGraphqlServer()));
+    app.use("/graphql", expressMiddleware(await createApolloGraphqlServer(), {
+        context: async ({req})=>{
+            const token = req.headers['token']
+            try {
+                const user = UserService.decodeJWTToken(token);
+                return {user};
+            }catch (error){
+                return {};
+            }
+        }
+    }));
     app.listen(PORT, ()=> console.log("Server is started on "+PORT))
 }
 
