9f5960a9e050dd4d4c92a44813ec1545bf47784d
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-06-23 17:10:31 +0500

    feat: update createUser mutation to accept a more flexible lastName parameter and return user ID

--- ThreadAppBackend/src/graphql/user/resolver.ts
+++ ThreadAppBackend/src/graphql/user/resolver.ts
@@ -1,8 +1,11 @@
+import UserService, {type CreateUserPayload} from "../../services/user.js";
+
 const queries = {};
 
 const mutations = {
-    createUser: async (_: any, {}: {}) => {
-     return "randomId"
+    createUser: async (_: any, payload: CreateUserPayload) => {
+        const res = await UserService.createUser(payload);
+        return res.id;
     }
 };
 
